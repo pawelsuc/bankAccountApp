059f3e7f16935f1119bf94fe13e001fe016fb118
Paweł Suchań <pawelsuchan94@gmail.com>
2023-05-19 08:31:03 +0200

    Generating the Account Number

--- src/Account.java
+++ src/Account.java
@@ -3,6 +3,7 @@ public abstract class Account implements Rate {
     String name;
     String sSN;
     double balance;
+    static int index = 10000;
     String accountNumber;
     double rate;
 
@@ -15,8 +16,19 @@ public abstract class Account implements Rate {
         balance = initDeposit;
         System.out.println("NAME: " + name + " SSN: " + sSN + " BALANCE: $" + balance);
 
-
-//    List common methods
+//    Set account number
+        index++;
+        this.accountNumber = setAccountNumber();
+        System.out.println("ACCOUNT NUMBER: " + this.accountNumber);
 
     }
+    private String setAccountNumber() {
+        String lastTwoOfSSN = sSN.substring(sSN.length()-2, sSN.length());
+        int uniqueId = index;
+        int randomNumber = (int) (Math.random() * Math.pow(10,3));
+        return lastTwoOfSSN + uniqueId + randomNumber;
+
+    }
+
+//    List common methods
 }
