f4221e67be39d5a9a33220a353c9c1513e33290b
Paweł Suchań <pawelsuchan94@gmail.com>
2023-05-19 15:19:45 +0200

    Implementing the Compound Interest Rate Method

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -2,12 +2,13 @@ public class BankAccountApp {
     public static void main(String[] args) {
         Checking checkingAccount1 = new Checking("Pablo Picasso","123456789", 1500);
         Savings savingAccount1 = new Savings ("Rich Lowe", "123456789", 2500);
+
+        savingAccount1.compound();
+
         checkingAccount1.showInfo();
         System.out.println("**********");
         savingAccount1.showInfo();
-        savingAccount1.deposit(5000);
-        savingAccount1.withdraw(200);
-        savingAccount1.transfer("Brokerage", 3000);
+
 
 
 //        Read a CSV File then create new accounts based on that data
