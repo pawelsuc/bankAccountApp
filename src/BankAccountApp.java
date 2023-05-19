3910f630ea920de330f6db1c14f285a6f86cefa4
Paweł Suchań <pawelsuchan94@gmail.com>
2023-05-19 16:23:46 +0200

    Writing a CSV Reader Utility

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -1,16 +1,30 @@
+import java.util.List;
+
 public class BankAccountApp {
     public static void main(String[] args) {
-        Checking checkingAccount1 = new Checking("Pablo Picasso","123456789", 1500);
-        Savings savingAccount1 = new Savings ("Rich Lowe", "123456789", 2500);
+        String file = "/home/pawelsuc/Pulpit/java/udemyTrustNsignCourses/Project3/NewBankAccounts.csv";
+//        Checking checkingAccount1 = new Checking("Pablo Picasso","123456789", 1500);
+//        Savings savingAccount1 = new Savings ("Rich Lowe", "123456789", 2500);
+//
+//        savingAccount1.compound();
+//
+//        checkingAccount1.showInfo();
+//        System.out.println("**********");
+//        savingAccount1.showInfo();
 
-        savingAccount1.compound();
+//        Read a CSV File then create new accounts based on that data
 
-        checkingAccount1.showInfo();
-        System.out.println("**********");
-        savingAccount1.showInfo();
 
 
 
 //        Read a CSV File then create new accounts based on that data
+        List<String[]> newAccountHolders = utilities.CSV.read(file);
+        for(String[] accountHolder : newAccountHolders) {
+            System.out.println("NEW ACCOUNT: ");
+            System.out.println(accountHolder[0]);
+            System.out.println(accountHolder[1]);
+            System.out.println(accountHolder[2]);
+            System.out.println(accountHolder[3]);
+        }
     }
 }
